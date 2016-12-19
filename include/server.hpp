51f6193c6d926bb32780d77bbd642c147c9d2e87
Balanar300hp <kudashov.vlad@yandex.ru>
2016-12-20 01:59:37 +0300

    Update server.hpp

--- include/server.hpp
+++ include/server.hpp
@@ -1,98 +1,28 @@
-#include <stdio.h>
-#include <openssl/rsa.h>
-#include <curl/curl.h>
-#include <openssl/x509v3.h>
-#include <openssl/x509_vfy.h>
-#include <openssl/crypto.h>
-#include <openssl/lhash.h>
-#include <openssl/evp.h>
-#include <openssl/objects.h>
-#include <openssl/err.h>
-#include <openssl/evp.h>
-#include <openssl/x509.h>
-#include <openssl/pkcs12.h>
-#include <openssl/bio.h>
-#include <openssl/ssl.h>
-#include <cstdio> 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-#include <algorithm>
-#include <fcntl.h>
-#include <boost/filesystem.hpp>
-#include <algorithm>
-#include <sstream>
-#define BUFSIZE 1024
-#include <boost/asio/io_service.hpp>
-#include <boost/bind.hpp>
-#include <boost/thread/thread.hpp>
-
-
-namespace bamthread
-{
-	typedef std::unique_ptr<boost::asio::io_service::work> asio_worker;
-
-	struct ThreadPool {
-		ThreadPool(size_t threads) :service(), working(new asio_worker::element_type(service)) {
-			while (threads--)
-			{
-				auto worker = boost::bind(&boost::asio::io_service::run, &(this->service));
-				g.add_thread(new boost::thread(worker));
-			}
-		}
-
-		template<class F>
-		void enqueue(F f) {
-			service.post(f);
-		}
-
-		~ThreadPool() {
-			working.reset();
-			g.join_all();
-			service.stop();
-		}
-
-	private:
-		boost::asio::io_service service;
-		asio_worker working;
-		boost::thread_group g;
-	};
-}
-
-struct Data
-{
-	char * buffer;
-	unsigned long long position;
-	unsigned long long size;
+struct YanDisk {
+	std::string login_ya, password_ya, path_ya;
+	std::vector<std::string> resources, temp_files;
 };
 
-namespace Download {
-	size_t stream(char * ptr, size_t item_size, size_t item_count, void * stream)
-	{
-		auto out_stream = (std::ostream *)stream;
-		size_t write_bytes = item_size * item_count;
-		out_stream->write(ptr, write_bytes);
-		return write_bytes;
-	}
-}
 
 class Server {
 public:
 	Server(std::string str);
-	auto Entry()->void;
-	void download_to(CURL *curl, std::string name, int i);
-	void aes_decrypt(int i);
+	auto Entry(int param)->void;//общая ф-я запуска программы для подключения и скачивания с сервера файлов двух структур
+	void download_to(CURL *curl, std::string name, int i, int param);//скачиваем с сервера
+	void aes_decrypt(int i, int param);//дешифруем
+	auto check_files(CURL *curl_,int param)->void;//поиск шифрованных файлов на яндекс диске
 private:
-	std::string server, login, password, files, client_file, path;
-	std::vector<std::string> file_names_dir, direct, files_download, temp_files;
+	std::string server, login, password, files, client_file, path, ya_d;
+	std::vector<std::string> file_names_dir, direct;
+	std::vector<YanDisk> structures;
+
 };
 
-void Server::aes_decrypt(int i)
+void Server::aes_decrypt(int i, int param)
 {
 	int outlen, inlen;
-	std::string s_out = path + "/out/" + files_download[i];
-	FILE *in = fopen(temp_files[i].c_str(), "rb"), *out = fopen(s_out.c_str(), "wb");
+	std::string s_out = structures[param].path_ya + "/out/" + structures[param].resources[i];
+	FILE *in = fopen(structures[param].temp_files[i].c_str(), "rb"), *out = fopen(s_out.c_str(), "wb");
 	unsigned char inbuf[BUFSIZE], outbuf[BUFSIZE];
 	unsigned char key[32]; /* 256- битный ключ */
 	unsigned char iv[8]; /* вектор инициализации */
@@ -117,33 +47,42 @@ void Server::aes_decrypt(int i)
 	EVP_CIPHER_CTX_cleanup(&ctx);
 	fclose(in);
 	fclose(out);
-	remove(temp_files[i].c_str());
+	remove(structures[param].temp_files[i].c_str());
 }
 
 Server::Server(std::string str) : client_file(str) {
 	std::ifstream file(client_file);
+	getline(file, ya_d);
 	getline(file, server);
-	getline(file, login);
-	getline(file, password);
-	getline(file, path);
-	std::string temp;
-	getline(file, temp);
-	std::istringstream iss(temp);
-	std::copy(std::istream_iterator<std::string>(iss),
-		std::istream_iterator<std::string>(),
-		std::back_inserter<std::vector<std::string> >(files_download));
+	int n = boost::lexical_cast<int> (ya_d);
+	
+	for (int i = 0; i < n; i++) {
+		YanDisk obj;
+		getline(file, obj.login_ya);
+		getline(file, obj.password_ya);
+		getline(file, obj.path_ya);
+		structures.push_back(obj);
+	}
+
+	/*Потоки на скачивание*/
+	bamthread::ThreadPool pp(2);
+	for (int i=0;i<n;i++) Entry(i);
 	file.close();
-	Entry();
 }
 
-void Server::download_to(CURL *curl_, std::string name, int i) {
+void Server::download_to(CURL *curl_, std::string name, int i, int param) {
 	std::ofstream file_stream(name, std::ios::binary);
 	if (file_stream.is_open()) {
-		std::string path_server = server + "/" + files_download[i];
+		/*Сбрасываем настройки сессии до исходных , подключаемся к серверу с новым url и скачиаем файл*/
+		curl_easy_reset(curl_);
+		curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYHOST, 0L);
+		curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(curl_, CURLOPT_VERBOSE, 1);
+		std::string path_server = server + "/" + structures[param].resources[i];
 		curl_easy_setopt(curl_, CURLOPT_URL, path_server.c_str());
 		curl_easy_setopt(curl_, CURLOPT_HTTPAUTH, (int)CURLAUTH_BASIC);
-		curl_easy_setopt(curl_, CURLOPT_USERNAME, login.c_str());
-		curl_easy_setopt(curl_, CURLOPT_PASSWORD, password.c_str());
+		curl_easy_setopt(curl_, CURLOPT_USERNAME, structures[param].login_ya.c_str());
+		curl_easy_setopt(curl_, CURLOPT_PASSWORD, structures[param].password_ya.c_str());
 		curl_easy_setopt(curl_, CURLOPT_CUSTOMREQUEST, "GET");
 		curl_easy_setopt(curl_, CURLOPT_HEADER, 0L);
 		curl_easy_setopt(curl_, CURLOPT_WRITEDATA, (size_t)&file_stream);
@@ -153,27 +92,68 @@ void Server::download_to(CURL *curl_, std::string name, int i) {
 	file_stream.close();
 }
 
-auto Server::Entry()->void {
+auto Server::Entry(int param)->void {
+	/*Подключаемся к серваку*/
 	CURL *curl_;
 	curl_ = curl_easy_init();
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYHOST, 0L);
 	curl_easy_setopt(curl_, CURLOPT_SSL_VERIFYPEER, 0L);
 	curl_easy_setopt(curl_, CURLOPT_VERBOSE, 1);
+	curl_easy_setopt(curl_, CURLOPT_URL, server.c_str());
+	curl_easy_setopt(curl_, CURLOPT_HTTPAUTH, (int)CURLAUTH_BASIC);
+	curl_easy_setopt(curl_, CURLOPT_USERNAME, structures[param].login_ya.c_str());
+	curl_easy_setopt(curl_, CURLOPT_PASSWORD, structures[param].password_ya.c_str());
+	curl_easy_perform(curl_);
+	/*Вызываем ф-ю подгрузки xml кода*/
+	check_files(curl_,param);
 
-
-	for (int i = 0; i < files_download.size(); i++) {
-		std::string t = path + "/temp/" + files_download[i];
-		temp_files.push_back(t);
-		download_to(curl_, t, i);
-		std::string path_server = server + "/" + files_download[i];
+	/*Начинаем скачивать файлы*/
+	for (int i = 0; i < structures[param].resources.size(); i++) {
+		std::string t = structures[param].path_ya + "/temp/" + structures[param].resources[i];
+		structures[param].temp_files.push_back(t);
+		download_to(curl_, t, i, param);
+		std::string path_server = server + "/" + structures[param].resources[i];
 		curl_easy_setopt(curl_, CURLOPT_CUSTOMREQUEST, "DELETE");
 		curl_easy_setopt(curl_, CURLOPT_URL, path_server.c_str());
 		curl_easy_perform(curl_);
 	}
 
-	bamthread::ThreadPool tp(files_download.size());
-	for (int i = 0; i < files_download.size(); i++) {
-		tp.enqueue(boost::bind(&Server::aes_decrypt, this, i));
+	/*Запускаем потоки дешифровки, выделяем 4 потока*/
+	bamthread::ThreadPool tp(4);
+	for (int i = 0; i < structures[param].resources.size(); i++) {
+		tp.enqueue(boost::bind(&Server::aes_decrypt, this, i, param));
+	}
+
+	
+	/*Чистим сессию*/
+	curl_global_cleanup();
+	curl_easy_reset(curl_);
+	curl_easy_cleanup(curl_);
+}
+
+auto Server::check_files(CURL *curl,int param)->void {
+	Header header = {
+		"Accept: */*",
+		"Depth: 1"
+	};
+	Data data = { 0, 0, 0 };
+	/*Подгружаем xml код по коду ищем тэг с именем файла , файл содержащий .enc на конце добавляем в вектор*/
+	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PROPFIND");
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, (struct curl_slist *)header.handle);
+	curl_easy_setopt(curl, CURLOPT_HEADER, 0);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (size_t)&data);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, (size_t)Append::buffer);
+	curl_easy_perform(curl);
+	size_t n;
+	/*Работа xml кодом из структуры data*/
+	pugi::xml_document document;
+	document.load_buffer(data.buffer, (size_t)data.size);
+	auto nodes = document.select_nodes("//d:displayname");
+	for (auto& node : nodes)
+	{
+		std::string ttt = node.node().text().as_string();
+		n = ttt.find(".enc");
+		if (n != std::string::npos) structures[param].resources.push_back(node.node().text().as_string());
 	}
 }
